0f743fe1e2fd97d3eacba6b88f395001b4a30510
Yuriy Osychenko <yuriy.osychenko@gmail.com>
2016-10-01 22:47:18 +0300

    Implemented game startup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-/**
- * Created by yura on 9/30/16.
- */
+import com.golden.gamedev.GameLoader;
+
+import java.awt.*;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+        GameApplication gameApplication = new GameApplication();
+        GameLoader loader = new GameLoader();
+        loader.setup(gameApplication, new Dimension(1280, 720), false);
+        loader.start();
+    }
 }
