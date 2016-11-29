6673339a8df5529bdf2e2a722dd38c91361ab48a
viktoriaSuntsova <viktoriasuntsova@gmail.com>
2016-11-29 20:22:03 +0300

    Add AIObstacleCollision

---
+++ src/collision/AIObstacleCollision.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collision;
+
+import com.golden.gamedev.object.Sprite;
+import com.golden.gamedev.object.collision.BasicCollisionGroup;
+
+/**
+ *
+ * @author Viktoria
+ */
+public class AIObstacleCollision extends BasicCollisionGroup{
+
+    @Override
+    public void collided(Sprite sprite, Sprite sprite1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
